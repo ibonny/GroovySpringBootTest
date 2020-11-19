0ec9bd458c44ce9cab43e2da54f66f000c210323
ibonny <ibonny@gmail.com>
2020-11-19 14:34:38 +0000

    Initial commit of Groovy code.

---
+++ src/main/groovy/personal/ibonny/groovyTest/DemoApplication.groovy
@@ -0,0 +1,13 @@
+package personal.ibonny.groovyTest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoApplication, args)
+	}
+
+}
---
+++ src/main/groovy/personal/ibonny/groovyTest/MainController.groovy
@@ -0,0 +1,36 @@
+package personal.ibonny.groovyTest
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+import org.springframework.stereotype.Controller
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.ValueNode
+
+@Controller
+public class MainController {
+    @GetMapping('/')
+    String getIndex() {
+        // ObjectMapper om = new ObjectMapper()
+
+        // def root = om.createObjectNode()
+
+        // root.put("result", "OK")
+
+        // return root
+
+        return "index.html"
+    }
+}
